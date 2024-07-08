b2d4265e6ed1a692c0fdb675d5952197fd4a2858
arupay <arupay11374@gmail.com>
2024-07-07 23:57:43 -0400

    completed cart view, and checkout components with notifications and mock backend

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  private apiUrl = environment.apiUrl + '/cart';
+  private checkoutUrl = environment.apiUrl + '/checkout';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getCart(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+  addToCart(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+  clearCart(): Observable<void> {
+    return this.http.delete<void>(this.apiUrl);
+  }
+  checkout(products: Product[]): Observable<void> {
+    return this.http.post<void>(this.checkoutUrl, products);
+  }
 }
